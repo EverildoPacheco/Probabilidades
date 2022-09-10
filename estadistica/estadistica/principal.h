a9f002fc8b6ec143142135946db18325cfc80f59
EverildoPacheco <jpachecot2@miumg.edu.gt>
2022-09-09 22:24:29 -0600

    Prueba 3

--- estadistica/estadistica/principal.h
+++ estadistica/estadistica/principal.h
@@ -35,6 +35,11 @@ namespace estadistica {
 			}
 		}
 	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::Label^ label6;
 	protected:
 
 	private:
@@ -51,6 +56,11 @@ namespace estadistica {
 		void InitializeComponent(void)
 		{
 			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->label6 = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
 			// 
 			// label1
@@ -58,17 +68,77 @@ namespace estadistica {
 			this->label1->AutoSize = true;
 			this->label1->Font = (gcnew System::Drawing::Font(L"Arial Rounded MT Bold", 16.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label1->Location = System::Drawing::Point(206, 35);
+			this->label1->Location = System::Drawing::Point(242, 39);
 			this->label1->Name = L"label1";
 			this->label1->Size = System::Drawing::Size(108, 32);
 			this->label1->TabIndex = 0;
 			this->label1->Text = L"MIGUE";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Arial Rounded MT Bold", 16.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->Location = System::Drawing::Point(78, 125);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(108, 32);
+			this->label2->TabIndex = 1;
+			this->label2->Text = L"MIGUE";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Arial Rounded MT Bold", 16.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label3->Location = System::Drawing::Point(242, 151);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(108, 32);
+			this->label3->TabIndex = 2;
+			this->label3->Text = L"MIGUE";
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Arial Rounded MT Bold", 16.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label4->Location = System::Drawing::Point(344, 274);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(108, 32);
+			this->label4->TabIndex = 3;
+			this->label4->Text = L"MIGUE";
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Font = (gcnew System::Drawing::Font(L"Arial Rounded MT Bold", 16.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label5->Location = System::Drawing::Point(122, 260);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(108, 32);
+			this->label5->TabIndex = 4;
+			this->label5->Text = L"MIGUE";
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->Font = (gcnew System::Drawing::Font(L"Arial Rounded MT Bold", 16.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label6->Location = System::Drawing::Point(399, 125);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(108, 32);
+			this->label6->TabIndex = 5;
+			this->label6->Text = L"MIGUE";
 			// 
 			// principal
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(578, 421);
+			this->Controls->Add(this->label6);
+			this->Controls->Add(this->label5);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
 			this->Controls->Add(this->label1);
 			this->Name = L"principal";
 			this->Text = L"principal";
