fcf879d15ad2066d0a2f3316f3a189b166f18bbb
miguel071 <miguelamilcarcr7@gmail.com>
2022-09-13 22:43:03 -0600

    Diseño de Inicio

--- estadistica/estadistica/principal.cpp
+++ estadistica/estadistica/principal.cpp
@@ -1,2 +1,13 @@
 #include "principal.h"
+using namespace estadistica;
+
+[STAThreadAttribute]
+
+int main(array<System::String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::Run(gcnew principal());
+	return 0;
+}
 
--- estadistica/estadistica/principal.h
+++ estadistica/estadistica/principal.h
@@ -35,6 +35,9 @@ namespace estadistica {
 			}
 		}
 	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Button^ button1;
 	protected:
 
 	private:
@@ -50,7 +53,12 @@ namespace estadistica {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(principal::typeid));
 			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// label1
@@ -58,20 +66,63 @@ namespace estadistica {
 			this->label1->AutoSize = true;
 			this->label1->Font = (gcnew System::Drawing::Font(L"Arial Rounded MT Bold", 16.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label1->Location = System::Drawing::Point(206, 35);
+			this->label1->Location = System::Drawing::Point(154, 28);
+			this->label1->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(108, 32);
+			this->label1->Size = System::Drawing::Size(0, 26);
 			this->label1->TabIndex = 0;
-			this->label1->Text = L"MIGUE";
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->ErrorImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.ErrorImage")));
+			this->pictureBox1->ImageLocation = L"C:\\Users\\Miguel 07\\source\\repos\\probabilidades\\estadistica\\imagenes\\1.png";
+			this->pictureBox1->InitialImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.InitialImage")));
+			this->pictureBox1->Location = System::Drawing::Point(51, 69);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(646, 398);
+			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
+			this->pictureBox1->TabIndex = 1;
+			this->pictureBox1->TabStop = false;
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Comic Sans MS", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->Location = System::Drawing::Point(186, 28);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(329, 38);
+			this->label2->TabIndex = 2;
+			this->label2->Text = L"Proyecto de Estadistica";
+			// 
+			// button1
+			// 
+			this->button1->Font = (gcnew System::Drawing::Font(L"Comic Sans MS", 9.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button1->ForeColor = System::Drawing::Color::LightCoral;
+			this->button1->Location = System::Drawing::Point(276, 172);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(68, 58);
+			this->button1->TabIndex = 3;
+			this->button1->Text = L"Iniciar";
+			this->button1->UseVisualStyleBackColor = true;
 			// 
 			// principal
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(578, 421);
+			this->BackColor = System::Drawing::Color::White;
+			this->ClientSize = System::Drawing::Size(808, 494);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->pictureBox1);
 			this->Controls->Add(this->label1);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
+			this->Margin = System::Windows::Forms::Padding(2);
 			this->Name = L"principal";
+			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
 			this->Text = L"principal";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
